21dfacb28cc04c76d0269452683ff96f63bdbb2e
Luis Herr <kontakt@luisherr.eu>
2026-02-14 00:02:29 +0100

    feat(chadtree): added feature chadtree

---
+++ config/ui/chadtree.nix
@@ -0,0 +1,17 @@
+{
+  plugins.chadtree = {
+    enable = true;
+    autoLoad = true;
+    settings = {
+      theme = {
+        icon_glyph_set = "devicons";
+        text_colour_set = "nerdtree_syntax_dark";
+      };
+      view = {
+        window_options = {
+          relativenumber = true;
+        };
+      };
+    }; 
+  };
+}
